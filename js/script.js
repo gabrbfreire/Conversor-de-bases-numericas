d0c435668536549972146646dafb42d14465933e
Gabriel Freire <gabrbfreire@gmail.com>
2019-09-14 19:00:11 -0300

    Visual changes

--- js/script.js
+++ js/script.js
@@ -1,257 +1,263 @@
 //Conversões Binário -> ...
-var BD = $('#entradaBD').parsley(); //Conecta Parsley ao input a ser validado
-$('#entradaBD').on('input',function(){ //Evento 'input' que é ativado quando qualquer valor é inserido no input
+var BD = $('#entrada').parsley(); //Conecta Parsley ao input a ser validado
+$('#entrada').on('input', function () { //Evento 'input' que é ativado quando qualquer valor é inserido no input
     var digitoFinal = this.value.length - 1; //Posição do último digito
     var entrada = this.value; //Valor do input antes de ser validado
-    if(BD.isValid()){ //Parsley verifica se o valor inserido é válido
+    if (BD.isValid()) { //Parsley verifica se o valor inserido é válido
         var resultado = binarioDecimal(entrada); //Se válido é feita a conversão
-        if($('#entradaBD').val().length === 0){ //Previne 'NaN' quando campo é apagado e apaga resultados
+        if ($('#entrada').val().length === 0) { //Previne 'NaN' quando campo é apagado e apaga resultados
             $('#resultadoBD').text("Decimal: ");
-        }else{
+        } else {
             $('#resultadoBD').text("Decimal: " + resultado); //Mostra resultado
         }
-    }else{  //Remove o último digito se inválido
-        this.value = entrada.slice(0,digitoFinal); //Retira último digito e muda 'value' do input
+    } else { //Remove o último digito se inválido
+        this.value = entrada.slice(0, digitoFinal); //Retira último digito e muda 'value' do input
     }
 });
 
-var BO = $('#entradaBO').parsley();
-$('#entradaBO').on('input',function(){
+var BO = $('#entrada').parsley();
+$('#entrada').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(BO.isValid()){
+    if (BO.isValid()) {
         var resultado = binarioDecimal(entrada).toString(8);
-        if($('#entradaBO').val().length === 0){
+        if ($('#entrada').val().length === 0) {
             $('#resultadoBO').text("Octal: ");
-        }else{
+        } else {
             $('#resultadoBO').text("Octal: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
-var BH = $('#entradaBH').parsley();
-$('#entradaBH').on('input',function(){
+var BH = $('#entrada').parsley();
+$('#entrada').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(BH.isValid()){
+    if (BH.isValid()) {
         var resultado = binarioDecimal(entrada).toString(16);
-        if($('#entradaBH').val().length === 0){
+        if ($('#entrada').val().length === 0) {
             $('#resultadoBH').text("Hexadecimal: ");
-        }else{
+        } else {
             $('#resultadoBH').text("Hexadecimal: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
 
 //Conversões Decimal -> ...
-var DB = $('#entradaDB').parsley();
-$('#entradaDB').on('input',function(){
+var DB = $('#entradaD').parsley();
+$('#entradaD').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(DB.isValid()){
+    if (DB.isValid()) {
         var resultado = decimalBinario(entrada);
-        if($('#entradaDB').val().length === 0){
+        if ($('#entradaD').val().length === 0) {
             $('#resultadoDB').text("Binário: ");
-        }else{
+        } else {
             $('#resultadoDB').text("Binário: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
-var DO = $('#entradaDO').parsley();
-$('#entradaDO').on('input',function(){
+var DO = $('#entradaD').parsley();
+$('#entradaD').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(DO.isValid()){
+    if (DO.isValid()) {
         var resultado = decimalOctal(entrada);
-        if($('#entradaDO').val().length === 0){
+        if ($('#entradaD').val().length === 0) {
             $('#resultadoDO').text("Octal: ");
-        }else{
+        } else {
             $('#resultadoDO').text("Octal: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
-var DH = $('#entradaDH').parsley();
-$('#entradaDH').on('input',function(){
+var DH = $('#entradaD').parsley();
+$('#entradaD').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(DH.isValid()){
+    if (DH.isValid()) {
         var resultado = decimalHexa(entrada);
-        if($('#entradaDH').val().length === 0){
+        if ($('#entradaD').val().length === 0) {
             $('#resultadoDH').text("Hexadecimal: ");
-        }else{
+        } else {
             $('#resultadoDH').text("Hexadecimal: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
 
 //Conversões Octal -> ...
-var OB = $('#entradaOB').parsley();
-$('#entradaOB').on('input',function(){
+var OB = $('#entradaO').parsley();
+$('#entradaO').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(OB.isValid()){
+    if (OB.isValid()) {
         var resultado = octalDecimal(entrada).toString(2);
-        if($('#entradaOB').val().length === 0){
+        if ($('#entradaO').val().length === 0) {
             $('#resultadoOB').text("Binário: ");
-        }else{
+        } else {
             $('#resultadoOB').text("Binário: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
-var OD = $('#entradaOD').parsley();
-$('#entradaOD').on('input',function(){
+var OD = $('#entradaO').parsley();
+$('#entradaO').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(OD.isValid()){
+    if (OD.isValid()) {
         var resultado = octalDecimal(entrada);
-        if($('#entradaOD').val().length === 0){
+        if ($('#entradaO').val().length === 0) {
             $('#resultadoOD').text("Decimal: ");
-        }else{
+        } else {
             $('#resultadoOD').text("Decimal: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
-var OH = $('#entradaOH').parsley();
-$('#entradaOH').on('input',function(){
+var OH = $('#entradaO').parsley();
+$('#entradaO').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(OH.isValid()){
+    if (OH.isValid()) {
         var resultado = octalDecimal(entrada).toString(16);
-        if($('#entradaOH').val().length === 0){
+        if ($('#entradaO').val().length === 0) {
             $('#resultadoOH').text("Hexadecimal: ");
-        }else{
+        } else {
             $('#resultadoOH').text("Hexadecimal: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
 
 //Conversões Hexadecimal -> ...
-var HB = $('#entradaHB').parsley();
-$('#entradaHB').on('input',function(){
+var HB = $('#entradaH').parsley();
+$('#entradaH').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(HB.isValid()){
+    if (HB.isValid()) {
         var resultado = hexaDecimal(entrada).toString(2);
-        if($('#entradaHB').val().length === 0){
+        if ($('#entradaH').val().length === 0) {
             $('#resultadoHB').text("Binário: ");
-        }else{
+        } else {
             $('#resultadoHB').text("Binário: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
-var HD = $('#entradaHD').parsley();
-$('#entradaHD').on('input',function(){
+var HD = $('#entradaH').parsley();
+$('#entradaH').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(HD.isValid()){
+    if (HD.isValid()) {
         var resultado = hexaDecimal(entrada);
-        if($('#entradaHD').val().length === 0){
+        if ($('#entradaH').val().length === 0) {
             $('#resultadoHD').text("Decimal: ");
-        }else{
+        } else {
             $('#resultadoHD').text("Decimal: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
-var HO = $('#entradaHO').parsley();
-$('#entradaHO').on('input',function(){
+var HO = $('#entradaH').parsley();
+$('#entradaH').on('input', function () {
     var digitoFinal = this.value.length - 1;
     var entrada = this.value;
-    if(HO.isValid()){
+    if (HO.isValid()) {
         var resultado = hexaDecimal(entrada).toString(8);
-        if($('#entradaHO').val().length === 0){
+        if ($('#entradaH').val().length === 0) {
             $('#resultadoHO').text("Octal: ");
-        }else{
+        } else {
             $('#resultadoHO').text("Octal: " + resultado);
         }
-    }else{
-        this.value = entrada.slice(0,digitoFinal);
+    } else {
+        this.value = entrada.slice(0, digitoFinal);
     }
 });
 
 
 // Funções de conversão
-function decimalBinario(entrada){
+function decimalBinario(entrada) {
     return parseInt(entrada).toString(2); //Transforma 'entrada' em número e em seguida converte para binário
 }
-function decimalOctal(entrada){
+
+function decimalOctal(entrada) {
     return parseInt(entrada).toString(8); //Transforma 'entrada' em número e em seguida converte para octal
 }
-function decimalHexa(entrada){
+
+function decimalHexa(entrada) {
     return parseInt(entrada).toString(16); //Transforma 'entrada' em número e em seguida converte para hexadecimal
 }
-function binarioDecimal(entrada){
-    return parseInt(entrada,2); //Converte 'entrada' de binário para decimal
+
+function binarioDecimal(entrada) {
+    return parseInt(entrada, 2); //Converte 'entrada' de binário para decimal
 }
-function octalDecimal(entrada){
-    return parseInt(entrada,8); //Converte 'entrada' de octal para decimal
+
+function octalDecimal(entrada) {
+    return parseInt(entrada, 8); //Converte 'entrada' de octal para decimal
 }
-function hexaDecimal(entrada){
-    return parseInt(entrada,16); //Converte 'entrada' de hexadecimal para decimal
+
+function hexaDecimal(entrada) {
+    return parseInt(entrada, 16); //Converte 'entrada' de hexadecimal para decimal
 }
 
 
-$('.abrirbtn').on('click',abrirNav);
-$('.fecharbtn').on('click',fecharNav);
+$('.abrirbtn').on('click', abrirNav);
+$('.fecharbtn').on('click', fecharNav);
 
 function abrirNav() {
-    $("#Sidenav").css('width','250px');
-    $("#main").css('marginLeft','250px');
+    $("#Sidenav").css('width', '250px');
+    $("#main").css('marginLeft', '250px');
 }
+
 function fecharNav() {
-    $("#Sidenav").css('width','0');
-    $("#main").css('marginLeft','0');
-} 
+    $("#Sidenav").css('width', '0');
+    $("#main").css('marginLeft', '0');
+}
 
 
 //Controle de grupos de conversão
-$("#botaoBinario").on('click',function(){
+$("#botaoBinario").on('click', function () {
     $("#grupoBinario").show();
     $("#grupoDecimal").hide();
     $("#grupoOctal").hide();
     $("#grupoHexa").hide();
 });
-$("#botaoDecimal").on('click',function(){
+$("#botaoDecimal").on('click', function () {
     $("#grupoBinario").hide();
     $("#grupoDecimal").show();
     $("#grupoOctal").hide();
     $("#grupoHexa").hide();
 });
-$("#botaoOctal").on('click',function(){
+$("#botaoOctal").on('click', function () {
     $("#grupoBinario").hide();
     $("#grupoDecimal").hide();
     $("#grupoOctal").show();
     $("#grupoHexa").hide();
 });
-$("#botaoHexa").on('click',function(){
+$("#botaoHexa").on('click', function () {
     $("#grupoBinario").hide();
     $("#grupoDecimal").hide();
     $("#grupoOctal").hide()
